43f0099172d524c7ec93e0821ce50aa9bd7708e8
Quentin PROUST <quentin.proust@serli.com>
2019-01-23 22:26:27 +0100

    parameterize plugin

--- plugin-gradle/src/main/groovy/com/serli/dojo/hello/HelloPlugin.groovy
+++ plugin-gradle/src/main/groovy/com/serli/dojo/hello/HelloPlugin.groovy
@@ -7,14 +7,21 @@ import org.gradle.api.Project
 class HelloPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
+        // Add the 'greeting' extension object
+        def extension = project.extensions.create('greeting', GreetingPluginExtension)
+        // Add a task that uses configuration from the extension object
+
         project.tasks.register("hello") {
             group = "Dojo"
             description = "Produces a greeting"
 
             doLast {
-                println(FigletFont.convertOneLine("Hello Serli Dojo"))
+                println(FigletFont.convertOneLine(extension.message))
             }
         }
     }
 }
 
+class GreetingPluginExtension {
+    String message = 'Hello from GreetingPlugin'
+}
