42572bfee8cb82964feddc5851dcf22664f2eb2c
Quentin PROUST <quentin.proust@serli.com>
2019-01-23 22:22:01 +0100

    create a plugin hello

--- plugin-gradle/src/main/groovy/com/serli/dojo/hello/HelloPlugin.groovy
+++ plugin-gradle/src/main/groovy/com/serli/dojo/hello/HelloPlugin.groovy
@@ -1,12 +1,20 @@
 package com.serli.dojo.hello
 
+import com.github.lalyos.jfiglet.FigletFont
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class HelloPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
+        project.tasks.register("hello") {
+            group = "Dojo"
+            description = "Produces a greeting"
 
+            doLast {
+                println(FigletFont.convertOneLine("Hello Serli Dojo"))
+            }
+        }
     }
 }
 
